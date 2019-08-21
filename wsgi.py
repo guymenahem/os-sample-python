ae968b4da76a6e4aeb05551298a6b0690b3afc80
guy <guy@localhost.localdomain>
2019-08-21 22:11:58 +0300

    Change to own commit

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Welcome to the IDFCTS small demo!"
 
 if __name__ == "__main__":
     application.run()
