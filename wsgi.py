c00bb1f6052461114039bc8bce941b568d23a339
guy <guy@localhost.localdomain>
2019-08-21 22:37:03 +0300

    Change return val

--- wsgi.py
+++ wsgi.py
@@ -3,10 +3,10 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
-    return "<h1>Welcome to the IDFCTS small demo! </h1>"
 
-    # return "<h1>Welcome to the IDFCTS small demo! </h1> <br> <iframe src="https://www.pc.co.il/news/287438/"></iframe>"
+    # return "<h1>Welcome to the IDFCTS small demo! </h1>"
+
+    return "<h1>Welcome to the IDFCTS small demo! </h1> <br> <iframe src="https://www.pc.co.il/news/287438/"></iframe>"
 
 if __name__ == "__main__":
     application.run()
