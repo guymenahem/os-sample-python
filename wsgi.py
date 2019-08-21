d827fe7fde6161ac9e887aba38a5ceae521df751
guy <guy@localhost.localdomain>
2019-08-21 22:15:53 +0300

    bigger font

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Welcome to the IDFCTS small demo!"
+    return "<h1>Welcome to the IDFCTS small demo! </h1>"
 
 if __name__ == "__main__":
     application.run()
