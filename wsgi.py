1f988be10aa94ebdfaf703b6b07a1451f397e129
guy <guy@localhost.localdomain>
2019-08-21 22:39:01 +0300

    change "

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ def hello():
 
     # return "<h1>Welcome to the IDFCTS small demo! </h1>"
 
-    return "<h1>Welcome to the IDFCTS small demo! </h1> <br> <iframe src="https://www.pc.co.il/news/287438/"></iframe>"
+    return "<h1>Welcome to the IDFCTS small demo! </h1> <br> <iframe src=\"https://www.pc.co.il/news/287438/\"></iframe>"
 
 if __name__ == "__main__":
     application.run()
