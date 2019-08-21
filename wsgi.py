9a996d177de5e535c1e1bcd464eb131727912325
guy <guy@localhost.localdomain>
2019-08-21 22:49:19 +0300

    change color

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,7 @@ def hello():
 
     # return "<h1>Welcome to the IDFCTS small demo! </h1>"
 
-    return "<h1>Welcome to the IDFCTS small demo! </h1> <br><br><br> <iframe src=\"https://www.pc.co.il/news/287438/\" width=\"1200\" height=\"600\" align=\"middle\"> ></iframe>"
+    return "<h1 style=\"color:blue;\">Welcome to the IDFCTS small demo! </h1> <br><br><br> <iframe src=\"https://www.pc.co.il/news/287438/\" width=\"1000\" height=\"600\"> ></iframe>"
 
 if __name__ == "__main__":
     application.run()
