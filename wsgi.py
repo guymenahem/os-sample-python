bbee94cf46ed02d0917f826f0b001b2e2ae390b4
guy <guy@localhost.localdomain>
2019-08-22 16:57:22 +0300

    1111

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
 
-    # return "<h1>Welcome to the IDFCTS small demo! </h1>"
+    return "<h1>Welcome to the IDFCTS small demo! </h1>"
 
-    return "<h1 style=\"color:blue;\">Welcome to the IDFCTS small demo! </h1> <br><br><br> <iframe src=\"https://www.pc.co.il/news/287438/\" width=\"1000\" height=\"600\"> ></iframe>"
+    # return "<h1 style=\"color:blue;\">Welcome to the IDFCTS small demo! </h1> <br><br><br> <iframe src=\"https://www.pc.co.il/news/287438/\" width=\"1000\" height=\"600\"> ></iframe>"
 
 if __name__ == "__main__":
     application.run()
